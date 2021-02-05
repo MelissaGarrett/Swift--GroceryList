69179c6ee106f6b2684e3affd6ca85d7f1f7d3f0
MelissaGarrett <melissa_garrett@hotmail.com>
2021-02-04 23:46:46 -0600

    Added the static information to the CategoriesTable vc.

--- GroceryList/CategoriesTableViewController.swift
+++ GroceryList/CategoriesTableViewController.swift
@@ -8,13 +8,18 @@
 import UIKit
 
 class CategoriesTableViewController: UITableViewController {
+    
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBOutlet weak var showListBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var deleteListBarButtonItem: UIBarButtonItem!
+    
     var storeName: String!
+    var stores = [StoreModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = storeName
-        
-        
     }
 }
